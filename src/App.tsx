0f5a9879052557d871ed2348ebaa6dd86681695c
Hidash-broto <hidash920@gmail.com>
2023-08-23 09:19:07 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { Box, Button, Checkbox, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {css} from './style'
+import { Toaster, toast } from 'react-hot-toast';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+
+function App() {
+  const [includeCapitalLetters, setIncludeCapitalLetters] = useState(false);
+  const [includeLowerLetters, setIncludeLoweLetters] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+  const [passwordLength, setPasswordLength] = useState(5);
+  const [password, setPassword] = useState('');
+  const [passCopied, setPassCopied] = useState(false);
+
+
+  const passGen = () => {
+    if (!includeCapitalLetters && !includeLowerLetters && !includeNumbers && !includeSymbols) {
+      return toast.error('Select atleast one option') 
+    } 
+    if (passwordLength < 1) {
+      return toast.error('provide Password length more than 0')
+    }
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let characters = lowercaseLetters;
+
+    if (includeCapitalLetters) characters += capitalLetters;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
+    if (includeLowerLetters) characters += lowercaseLetters;
+
+    let generatedPassword = '';
+    for (let i = 0; i <= passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      generatedPassword += characters.charAt(randomIndex);
+    }
+    setPassword(generatedPassword)
+    let element: HTMLElement | null = document.getElementById('password');
+    element ? element.innerHTML = generatedPassword : element = null;
+  }
+  const copyToClipboard = () => {
+    setPassCopied(true)
+    navigator.clipboard.writeText(password);
+    setTimeout(() => {
+      setPassCopied(false)
+    }, 8000)
+  };
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  return (
+    <>
+    <Toaster position="top-center"/>
+      <Grid direction='column' container sx={css.mainContainer}>
+        <Container sx={css.passwordContainer}>
+          <Box sx={css.displayContainer}>
+            <Typography id='password' sx={{mt: 1, color: '#55595e'}} variant='h5'>Password</Typography>
+          </Box>
+          <Button onClick={copyToClipboard} sx={css.clipboardContainer}>
+              {
+                passCopied ? <DoneAllIcon /> : <ContentCopyIcon />
+              }
+          </Button>
+        </Container>
+        <Container sx={css.controllersContainer}>
+              <Grid container spacing={2} sx={{padding: '20px', mt: 0}}>
+              <Grid item xs={6} sx={css.optionContainer}>
+              <Checkbox onChange={() => setIncludeCapitalLetters(!includeCapitalLetters)} {...label} />
+              <Typography sx={{fontSize: '20px'}}>Include Uppercase</Typography>
+              </Grid>
+              <Grid item xs={6} sx={css.optionContainer}>
+              <Checkbox onChange={() => setIncludeLoweLetters(!includeLowerLetters)} {...label} />
+              <Typography sx={{fontSize: '20px'}}>Include Lowercase</Typography>
+              </Grid>
+              <Grid item xs={6} sx={css.optionContainer}>
+              <Checkbox onChange={() => setIncludeNumbers(!includeNumbers)} {...label} />
+              <Typography sx={{fontSize: '20px'}}>Include Numbers</Typography>
+              </Grid>
+              <Grid item xs={6} sx={css.optionContainer}>
+              <Checkbox onChange={() => setIncludeSymbols(!includeSymbols)} {...label} />
+              <Typography sx={{fontSize: '20px'}}>Include Symbols</Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={6} sx={css.mainOptionContainer}>
+              <TextField onChange={(e: React.ChangeEvent<HTMLInputElement> | String | any) => setPasswordLength(e.target.value)} value={passwordLength} sx={css.lengthTextfield} type='number' id="outlined-basic" label="Length" variant="outlined" />
+              <Button onClick={passGen} sx={css.buttonStyle} variant='contained'>Generate</Button>
+              </Grid>
+        </Container>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
---
+++ src/style.ts
@@ -0,0 +1,68 @@
+export {}
+export const css = {
+    mainContainer: {
+        backgroundColor: 'black',
+        width: '100%',
+        height: '100%',
+        paddingBottom: '116px'
+    },
+    passwordContainer: {
+        backgroundColor: 'white',
+        height: '100px',
+        width: {
+            lg: '650px',
+            md: '400px',
+            xs: '200px'
+        },
+        marginTop: '120px',
+        borderRadius: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItem: 'center',
+        paddingTop: '25px',
+        filter: 'drop-shadow(0 0 0.75rem crimson)'
+    },
+    controllersContainer: {
+        backgroundColor: 'whitesmoke',
+        width: '650px',
+        height: '250px',
+        marginTop: '50px',
+        borderRadius: '10px',
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    optionContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '50px',
+    },
+    lengthTextfield: {
+        width: '100px',
+        height: '20px !important',
+        padding: '0px !important',
+    },
+    buttonStyle: {
+         ml: 1,
+         height: '40px'
+    },
+    mainOptionContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '50px',
+        marginLeft: '160px'
+    },
+    displayContainer: {
+        width: '500px',
+        height: '50px',
+        backgroundColor: '#b3cbf2',
+        borderRadius: '5px',
+        textAlign: 'center',
+    },
+    clipboardContainer: {
+        backgroundColor: 'rgba(48, 226, 12, 0.745)',
+        width: '50px',
+        height: '50px',
+        ml: 3
+    }
+  };
+  
